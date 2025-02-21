4fab444754a6635f8f99cdb1c3aecbb7f7397ce8
Brooke Wu <brookewu2023@gmail.com>
2025-02-20 19:27:51 -0500

    Home page now displays all medication entries in the schedule's entry list

--- MediReminder/app/(tabs)/index.tsx
+++ MediReminder/app/(tabs)/index.tsx
@@ -1,34 +1,23 @@
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 import UserSchedule from '../lib/userSchedule';
-import MedicationEntry from '../lib/medicationEntry';
-
-function EntryListComponent(entryList: MedicationEntry[]) {
-  if (entryList.length == 0) {
-    return (
-      <View style={styles.container}>
-        <Text>Your schedule is currently empty</Text>
-      </View>
-    )
-  }
-  else {
-    let entryListComponents = entryList.map((entry) => <li key={entry.name}><Text>{entry.name}: {entry.amount}</Text></li>)
-    return (
-      <View style={styles.container}>
-        <ul>
-          {entryListComponents}
-        </ul>
-      </View>
-    );
-  }
-}
 
 export default function HomeScreen() {
   // Use the useState hook from React to handle updating the component when the schedule is changed
+  // React will re-render a component whenever its state changes
   const [refresh, setRefresh] = useState(false);
 
-  // Placeholder variable to keep track of the current entry list length, used to ensure that the refreshing logic is working correctly
-  let content: number = UserSchedule.getUserSchedule().entryList.length;
+  // Get the entry list from the singular UserSchedule instance
+  let entryList = UserSchedule.getUserSchedule().entryList;
+  // If the schedule contains 0 entries, the home screen should display a special message indicating that the schedule is empty
+  let entryListComponents;
+  if (entryList.length == 0) {
+    entryListComponents = "Your schedule is currently empty"
+  }
+  // Otherwise, list all of the entries in the schedule's entry list
+  else {
+    entryListComponents = entryList.map((entry) => `${entry.name}: ${entry.amount}\n`)
+  }
 
   // Function to toggle the refresh state
   const handleRefresh = () => {
@@ -37,7 +26,8 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <Text>{content}</Text>
+      {entryList.length == 0 ? "" : <Text>Current Schedule contains {entryList.length} entries</Text>}
+      <Text>{entryListComponents}</Text>
       {/* Button to manually refresh the page, need to figure out how to make this a relatively less tedious process (i.e. scroll up and hold to reload page) instead */}
       <TouchableOpacity onPress={handleRefresh}><Text>Refresh</Text></TouchableOpacity>
     </View>
